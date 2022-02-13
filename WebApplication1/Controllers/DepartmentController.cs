4ef9c83e53c261899472a1443cba3e5925ab4b45
JuanMLima <99377147+JuanMLima@users.noreply.github.com>
2022-02-13 19:49:29 -0300

    APIs for Depart Screen: POST, PUT and DELETE
    Create Methods

--- WebApplication1/Controllers/DepartmentController.cs
+++ WebApplication1/Controllers/DepartmentController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -19,10 +20,10 @@ namespace WebApplication1.Controllers
                     dbo.Department
                     ";
             DataTable table = new DataTable();
-            using(var con= new SqlConnection(ConfigurationManager.
+            using (var con = new SqlConnection(ConfigurationManager.
                 ConnectionStrings["EmployeeAppDB"].ConnectionString))
-                using(var cmd= new SqlCommand(query, con))
-                using(var da= new SqlDataAdapter(cmd))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
             {
                 cmd.CommandType = CommandType.Text;
                 da.Fill(table);
@@ -32,5 +33,97 @@ namespace WebApplication1.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, table);
 
         }
+   
+        public string Post(Department dep)
+        {
+            try
+            {
+                string query = @"
+                            insert into dbo.Department values
+                            ('" + dep.DepartmentName + @"')
+                            ";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+
+            }
+
+            return "Added Sucessfully!!";
+
+        }
+            catch (Exception)
+            {
+
+                return "Failed to Add";
+            }
+        }
+
+
+        public string Put(Department dep)
+        {
+            try
+            {
+                string query = @"
+                            update dbo.Department set DepartmentName=
+                            '"+dep.DepartmentName + @"'
+                            where DepartmentId="+dep.DepartmentId+@"
+                            ";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+
+                }
+
+                return "Updated Sucessfully!!";
+
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Update";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"
+                            delete from sbo.Department
+                            where DepartmentId="+id+@"
+                            ";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+
+                }
+
+                return "Deleted Sucessfully!!";
+
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Delete!!";
+            }
+        }
+
     }
 }
+
+   
